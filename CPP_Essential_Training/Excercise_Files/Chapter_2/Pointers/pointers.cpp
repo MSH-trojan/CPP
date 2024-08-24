827212f14925255926557f9b3d1718818bc023d9
MSH <157653802+MSH-trojan@users.noreply.github.com>
2024-08-24 18:05:58 -0400

    Add files via upload

---
+++ CPP_Essential_Training/Excercise_Files/Chapter_2/Pointers/pointers.cpp
@@ -0,0 +1,15 @@
+#include <format>
+#include <iostream>
+
+using std::format;
+using std::cout;
+
+int main() {
+    int x {7};
+    int y {42};
+    int* ip = &x;
+
+    cout << format("The value of x is {}\n", x);
+    cout << format("The value of y is {}\n", y);
+    cout << format("The value of *ip is {}\n", *ip);
+}
